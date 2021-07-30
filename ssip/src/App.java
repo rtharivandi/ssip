7abc27895f0868b5b92e3d645538936e1a679d1c
Achmad Raihan Tharivandi <tharivandi@outlook.com>
2021-07-30 23:14:57 +0200

    Added loop for commands

--- ssip/src/App.java
+++ ssip/src/App.java
@@ -1,66 +1,159 @@
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList; 
-
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
 
+    private static ArrayList<String> images = new ArrayList<>();
+    private static int currentSlide = 0;
+
     public App() {
-        SwingUtilities.invokeLater(new Runnable(){
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                
-                //Setting the frame of the program
+
+                // Setting the frame of the program
                 JFrame frame = new JFrame("SSIP");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
-                //Creating the buffered image
-                BufferedImage img = null;
+
+                BufferedImage bufferedImage = null;
 
                 try {
-                    //Trying to get the fill the BufferedImage object with an actual image
-                    //Which we need to show error just in case that it's not found
-                    img = ImageIO.read(getClass().getResource("fubuki.JPG"));
+                    bufferedImage = ImageIO.read(getClass().getResource(images.get(currentSlide)));
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    System.err.println("File error.");
                 }
-
-                ImageIcon imageIcon = new ImageIcon(img);
+    
+                ImageIcon imageIcon = new ImageIcon(bufferedImage);
                 JLabel label = new JLabel();
+    
+                // Set icon is the command that changes the picture
                 label.setIcon(imageIcon);
-
+    
                 frame.getContentPane().add(label, BorderLayout.CENTER);
                 frame.pack();
-
-                //Centering the frame
+    
+                // Centering the frame
                 frame.setLocationRelativeTo(null);
-
+    
+                // Making it visible
                 frame.setVisible(true);
+                
+                }
 
-            }
+
         });
+
+        
+    }
+
+    public void nextImage() {
+        currentSlide = (currentSlide + 1) % images.size();
     }
 
     public static void main(String[] args) throws Exception {
-       
-        private ArrayList<String> folders = new ArrayList();
-        
-        if (args[0].equals("a")) {
-            //System.out.println("Entering the .");
-            folders.add(args[1]);
-        } 
 
-        if (args[0].equals("p")) {
-            new App();
+        while (true) {
+            System.out.println("Please enter a command!");
+            System.out.println("p : Play the slideshow.\na : Add a new file.");
+            System.out.println("c : Clear the queue\n");
+
+            //Scanner scanner = new Scanner(System.in);
+            char command = new Scanner(System.in).next().charAt(0);
+
+            if (command == 'a') {
+                System.out.println("Please enter the path of the pic.");
+                String path = new Scanner(System.in).next();
+                System.out.println(path);
+                images.add(path);
+                System.out.println("File succesfully added.");
+            } else if (command == 'p') {
+                System.out.println(images.get(currentSlide));
+                new App();
+            } else if (command == 'c') {
+                images.clear();
+            } else if (command == 'n') {
+                currentSlide++;  
+            } else if (command == 'e') {
+                return;
+            } else {
+                System.out.println("Please enter a valid command.\n\n");
+            }
         }
 
+    
+
+        //  if (args[0].equals("a")) {
+        //     ObjectInputStream in = null;
+        //     try {
+        //         in = new ObjectInputStream(new FileInputStream("imagecache.ser"));
+        //     } finally {
+        //         System.out.println(args[1] + "\n");
+        //         images.add(args[1]);
+
+        //         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("imagecache.ser"));
+        //         out.writeObject(images);
+        //         out.flush();
+        //         out.close();
+        //         System.out.println("File succesfully added!");
+        //     }
+
+            
+        // }
+
+        // else if (args[0].equals("p")) {
+        //    try {
+        //     new App();
+        //    } catch (IndexOutOfBoundsException e) {
+        //        System.err.println("No files loaded.");
+        //    }
+        // } 
+
+        // else if (args[0].equals("n")) {
+        //     if (currentSlide == images.size()-1) {
+        //         System.err.println("Cannot go further, please add more pictures.");
+        //     } else {
+        //         currentSlide++;
+        //     }
+        // }
+
+
+        // else if (args[0].equals("am")) {
+        //     for (int i = 1; i < args.length; i++) {
+        //         try {
+        //             images.add(ImageIO.read(new File(args[i])));
+        //         } catch (IOException e) {
+        //             System.err.println("File " + args[i] + " not found!");
+        //         }
+        //     }
+        // } 
+
+        // else if (args[0].equals("c")) {
+        //     images.clear();
+        // } 
+
+        // // else if (args[0].equals("s")) {
+        // //     for (ImageIcon imageIcon : images) {
+        // //         System.out.println(imageIcon.get);
+        // //     }
+        // // }
+
+        // else {
+        //     System.out.println("Please enter a valid command!");
+        }
 
     }
-}
+
