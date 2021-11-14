eb9804fc53e109c3beeb0417a21c2d6e1bc90237
r_tharivandi <a.tharivandi@tum.de>
2021-11-14 12:21:18 +0100

    New, better method to add pictures into the queue

--- ssip/src/App.java
+++ ssip/src/App.java
@@ -9,16 +9,19 @@ import javax.swing.UnsupportedLookAndFeelException;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App extends JFrame {
 
+    private final static int WIDTH = 1920;
+    private final static int HEIGHT = 1080;
+
     private static ArrayList<String> images = new ArrayList<>();
-    private static int currentSlide = -1;
+    private static int currentSlide = 0;
 
     private static JLabel slidesLabel = new JLabel();
     private static ArrayList<Icon> icons = new ArrayList<>();
@@ -26,17 +29,10 @@ public class App extends JFrame {
     public App() {
         slidesLabel.setVerticalAlignment(JLabel.CENTER);
         slidesLabel.setHorizontalAlignment(JLabel.CENTER);
-        slidesLabel.setPreferredSize(new Dimension(1920, 1080));
+        slidesLabel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setLayout(new BorderLayout());
         add(slidesLabel, BorderLayout.CENTER);
         slidesLabel.addMouseListener(new ClickListener());
-
-//        try {
-//            Thread.sleep(1500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        nextImage();
     }
 
     public void nextImage() {
@@ -48,51 +44,50 @@ public class App extends JFrame {
 
         while (true) {
             System.out.println("Please enter a command!");
-            System.out.println("p : Play the slideshow.\na : Add a new file.");
-            System.out.println("c : Clear the queue");
+            System.out.println("p : Play the slideshow.\na : Add a new file.\nc : Clear the queue");
 
-            // Scanner scanner = new Scanner(System.in);
             char command = new Scanner(System.in).next().charAt(0);
 
             if (command == 'a') {
                 System.out.println("Please enter the path of the pic.");
                 String path = new Scanner(System.in).next();
 
-                File readFile = new File(path);
+                File file = new File(path);
 
                 try {
-                    if(readFile.isDirectory()) {
-                        System.out.println("Folder added!");
-                        insertFilesFolder(readFile);
+                    if(file.isDirectory()) {
+                        insertFilesFolder(file);
                     } else {
                         //TODO:Do the resizing thing with single files as well!
-                        icons.add(new ImageIcon(ImageIO.read(new File(path))));
+                        insertFileToQueue(file);
                     }
-                    System.out.println("File succesfully added.");
+                    System.out.println("File(s) succesfully added.\n");
                 } catch (IOException e) {
-                    System.out.println("File unsuccessfully added\nPlease try again.");
+                    System.out.println("Error. Please try again.\n");
                 }
 
-
             } else if (command == 'p') {
-            
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-                                | UnsupportedLookAndFeelException ex) {
-                            ex.printStackTrace();
-                        }
+                if (icons.isEmpty()) {
+                    System.out.println("No files in queue, please add some.");
+                } else {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                                    | UnsupportedLookAndFeelException ex) {
+                                ex.printStackTrace();
+                            }
 
-                        App app = new App();
-                        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        app.pack();
-                        app.setLocationRelativeTo(null);
-                        app.setVisible(true);
-                    }
-                });
+                            App app = new App();
+                            app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            app.pack();
+                            app.setLocationRelativeTo(null);
+                            app.setVisible(true);
+                        }
+                    });
+                }
             } else if (command == 'c') {
                 images.clear();
             } else if (command == 'e') {
@@ -104,27 +99,17 @@ public class App extends JFrame {
     }
 
     public static void insertFilesFolder (File folder) throws IOException {
-        for (File file : folder.listFiles()) {
+        System.out.println("Loading...");
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
             if (!file.isDirectory() && ImageIO.read(file) != null) {
-
-                BufferedImage bufferedImage = null;
-
-                try {
-                    bufferedImage = ImageIO.read(file);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                //TODO:Deal with non 1920x1080 images
-                //TODO: Find a way to stretch the JLabel to the size of the current pic!
-
-                Image resizedImage = bufferedImage.getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
-                ImageIcon imageIcon = new ImageIcon(resizedImage);
-                icons.add(imageIcon);
+                insertFileToQueue(file);
             }
         }
     }
 
+    public static void insertFileToQueue (File file) {
+        icons.add(new ImageIcon(new ImageIcon(file.getPath()).getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT)));
+    }
 
     public class ClickListener extends MouseAdapter {
         @Override
