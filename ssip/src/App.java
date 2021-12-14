b044cec517926d693a53e90eae042984039a84cd
24rai <24.raihan@gmail.com>
2021-12-14 22:50:13 +0100

    Added FileChooser, faster imports of files

--- ssip/src/App.java
+++ ssip/src/App.java
@@ -1,30 +1,28 @@
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App extends JFrame {
 
     private final static int WIDTH = 1920;
     private final static int HEIGHT = 1080;
 
-    private static final ArrayList<String> images = new ArrayList<>();
-    private static int currentSlide = 0;
+    private static int currentSlide = 1;
 
     private static final JLabel slidesLabel = new JLabel();
-    private static final ArrayList<Icon> icons = new ArrayList<>();
+    private static final ArrayList<File> images = new ArrayList<>();
 
     public App() {
         slidesLabel.setVerticalAlignment(JLabel.CENTER);
@@ -36,58 +34,89 @@ public class App extends JFrame {
     }
 
     public void nextImage() {
-        currentSlide = (currentSlide + 1) % icons.size();
-        slidesLabel.setIcon(icons.get(currentSlide));
+        currentSlide = (currentSlide + 1) % images.size();
+        File file = images.get(currentSlide);
+        try {
+            BufferedImage bufferedImage = ImageIO.read(file);
+            slidesLabel.setIcon(new ImageIcon(bufferedImage));
+        } catch (IOException e) {
+            System.err.println("File error, please try again!");
+        }
+    }
+
+    public static void startSlideshow(App app) {
+        if (images.isEmpty()) {
+            System.out.println("No files in queue, please add some.");
+        } else {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                            | UnsupportedLookAndFeelException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    app.pack();
+                    app.setLocationRelativeTo(null);
+                    app.setVisible(true);
+                }
+            });
+        }
+    }
+
+    public static void insertFilesFolder(File folder) throws IOException {
+        System.out.println("Loading...");
+        images.addAll(Arrays.asList(Objects.requireNonNull(folder.listFiles())));
+        images.removeIf(App::isNotImage);
+    }
+
+    public static boolean isNotImage (File file) {
+        try {
+            return !Files.probeContentType(file.toPath()).split("/")[0].equals("image");
+        } catch (IOException | NullPointerException e) {
+            return false;
+        }
+    }
+
+    public static void selectFiles() {
+        System.out.println("Please choose the path of the pic.");
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            try {
+                insertFilesFolder(file);
+                System.out.println("File(s) succesfully added.\n");
+            } catch (IOException e) {
+                System.out.println("Error. Please try again.\n");
+            }
+        }
+    }
+
+    public class ClickListener extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            nextImage();
+        }
     }
 
     public static void main(String[] args) throws Exception {
 
         while (true) {
             System.out.println("Please enter a command!");
-            System.out.println("p : Play the slideshow.\na : Add a new file.\nc : Clear the queue");
+            System.out.println("p : Play the slideshow.\na : Add a new file.\nc : Clear the queue\ne: End the program");
 
             char command = new Scanner(System.in).next().charAt(0);
 
             if (command == 'a') {
-                System.out.println("Please enter the path of the pic.");
-                String path = new Scanner(System.in).next();
-
-                File file = new File(path);
-
-                try {
-                    if(file.isDirectory()) {
-                        insertFilesFolder(file);
-                    } else {
-                        //TODO:Do the resizing thing with single files as well!
-                        insertFileToQueue(file);
-                    }
-                    System.out.println("File(s) succesfully added.\n");
-                } catch (IOException e) {
-                    System.out.println("Error. Please try again.\n");
-                }
-
+                selectFiles();
             } else if (command == 'p') {
-                if (icons.isEmpty()) {
-                    System.out.println("No files in queue, please add some.");
-                } else {
-                    SwingUtilities.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-                                    | UnsupportedLookAndFeelException ex) {
-                                ex.printStackTrace();
-                            }
-
-                            App app = new App();
-                            app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                            app.pack();
-                            app.setLocationRelativeTo(null);
-                            app.setVisible(true);
-                        }
-                    });
-                }
+                App app = new App();
+                startSlideshow(app);
             } else if (command == 'c') {
                 images.clear();
             } else if (command == 'e') {
@@ -98,26 +127,8 @@ public class App extends JFrame {
         }
     }
 
-    public static void insertFilesFolder (File folder) throws IOException {
-        System.out.println("Loading...");
-        for (File file : Objects.requireNonNull(folder.listFiles())) {
-            if (!file.isDirectory() && ImageIO.read(file) != null) {
-                insertFileToQueue(file);
-            }
-        }
-    }
-
-    public static void insertFileToQueue (File file) {
-        icons.add(new ImageIcon(new ImageIcon(file.getPath()).getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT)));
-    }
-
-    public class ClickListener extends MouseAdapter {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            nextImage();
-        }
-    }
-
 }
+
+
 
 
